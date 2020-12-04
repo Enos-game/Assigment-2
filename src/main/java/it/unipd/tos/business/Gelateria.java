dcb68d99320ad63841bcf1d46a2bc7feeb5205ac
Enos-game <mirco.botta11@gmail.com>
2020-12-04 10:03:10 +0100

    close #2 aggiunta nel file Gelateria possibilità scontare nel caso di acquisto di piu di 5 gelati

--- src/main/java/it/unipd/tos/business/Gelateria.java
+++ src/main/java/it/unipd/tos/business/Gelateria.java
@@ -14,6 +14,7 @@ public class Gelateria implements TakeAwayBill{
         double Cibo = 0.0;
         double Bevande = 0.0;
         int nrGelati = 0;
+        double GelatoEconomico = 100.0;
         for(MenuItem menuItem : itemsOrder) {
             if(menuItem.getType() == items.Bevande) {
                 Bevande += menuItem.getPrice();
@@ -21,6 +22,15 @@ public class Gelateria implements TakeAwayBill{
             else {
                 Cibo += menuItem.getPrice(); 
             }
+            if(menuItem.getType() == items.Gelati) {
+                nrGelati++;
+                if(GelatoEconomico > menuItem.getPrice()) {
+                    GelatoEconomico = menuItem.getPrice();
+                }
+            }
+        }
+        if(nrGelati > 5) {
+            Cibo -= (GelatoEconomico/2);
         }
         return Cibo + Bevande;
     }
